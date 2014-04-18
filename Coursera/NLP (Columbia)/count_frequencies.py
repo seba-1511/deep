5c83af593f617a16c4e2cb2c8784bd5d26cee350
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-04-18 09:41:48 -0700

    Added general n_gram function (values slightly off, need to fix)

--- Coursera/NLP (Columbia)/count_frequencies.py
+++ Coursera/NLP (Columbia)/count_frequencies.py
@@ -16,19 +16,39 @@ def word_count():
     f.close()
     return wordCountDict
 
-def one_gram(wordCountDict):
+def n_gram(n):
 
-    I_GENE = 0
-    ZERO   = 0
+    f = open('gene.train','r')
+    lines = [line.split() for line in f.readlines()]
 
-    for word in wordCountDict:
-        if word[1] == 'I-GENE':
-            I_GENE += wordCountDict[word]
+    sentence = []
+    sentenceList = []
+    oneGramDict = defaultdict(int)
+
+    for line in lines:
+
+        if line != []:
+            sentence.append(line)
         else:
-            ZERO += wordCountDict[word]
+            sentence.insert(0,['Start','*'])
+            sentence.append(['End','STOP'])
+            sentenceList.append(sentence)
+            sentence = []
 
-    print ZERO
-    print I_GENE
+    for sentence in sentenceList:
+
+        currentGram = []
+
+        for word in sentence:
+            currentGram.append(word[1])
+
+            if len(currentGram) == n:
+                oneGramDict[tuple(currentGram)] += 1
+                currentGram.pop(0)
+
+    for word in oneGramDict:
+
+        print oneGramDict[word], "1-GRAM", word
 
 if __name__ == '__main__':
 
@@ -37,4 +57,4 @@ if __name__ == '__main__':
     #for word in wordCountDict:
     #   print wordCountDict[word], "WORDTAG", word[1], word[0]
 
-    one_gram(word_count())
+    n_gram(1)
