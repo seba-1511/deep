c115baf4367d8b20792524dd08b420185e53b368
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-04-23 20:51:24 -0700

    Added sentence tokenizer. Need to clean newlines from raw text

--- N-Gram/n_gram.py
+++ N-Gram/n_gram.py
@@ -2,28 +2,45 @@ import sys
 sys.path.append("/Users/gabrielpereyra/anaconda/lib/python2.7/site-packages")
 import nltk
 
-f = open('reuters21578/reut2-000.sgm','r')
+def tokenize_sent(text):
+    sent_detector = nltk.load('tokenizers/punkt/english.pickle')
 
-raw_text = f.readlines()[:100]
+    return sent_detector.tokenize(text.strip())
 
-body_text = []
-current_body = []
-in_body = False
+def extract_body_text(raw_text):
 
-for line in raw_text:
+    body_text = []
+    current_body = []
+    in_body = False
 
-    if in_body == True:
-        if line.find('</BODY>') > 0:
-            end_index = line.find('<BODY>')
-            current_body.append(line[:end_index])
-            body_text.append(current_body[:-2]) # -2 to strip out "Reuter" and "&#3;</BODY></TEXT>"
-            current_body = []
-            in_body = False
-        else:
-            current_body.append(line)
+    for line in raw_text:
 
-    if in_body == False:
-        if line.find('<BODY>') > 0:
-            start_index = line.find('<BODY>')
-            current_body.append(line[start_index+len('<BODY>'):])
-            in_body = True
+        if in_body == True:
+            if line.find('</BODY>') > 0:
+                end_index = line.find('<BODY>')
+                current_body.append(line[:end_index])
+                body_text.append(current_body[:-2]) # -2 to strip out "Reuter" and "&#3;</BODY></TEXT>"
+                current_body = []
+                in_body = False
+            else:
+                current_body.append(line)
+
+        if in_body == False:
+            if line.find('<BODY>') > 0:
+                start_index = line.find('<BODY>')
+                current_body.append(line[start_index+len('<BODY>'):])
+                in_body = True
+
+    return body_text
+
+if __name__ == '__main__':
+
+    f = open('reuters21578/reut2-000.sgm','r')
+
+    raw_text = f.readlines()[:100]
+
+    text = extract_body_text(raw_text)
+
+    sentences = tokenize_sent(' '.join(text[0]))
+
+    print sentences[0]
