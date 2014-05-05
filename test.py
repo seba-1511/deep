72c29f1d564bf31ced1e74fe2766a4240dc74db4
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-05-05 14:24:08 -0700

    Added multiple train subject loading

--- test.py
+++ test.py
@@ -6,31 +6,41 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from scipy.io import loadmat
 
-# load train x and y
-train_data = loadmat('train_01_06/train_subject01.mat', squeeze_me=True)
-train_x = train_data['X']
-train_y = train_data['y']
+def create_features(x):
+    x = x.reshape(x.shape[0], x.shape[1] * x.shape[2])
+    x -= x.mean(0)
+    x = np.nan_to_num(x / x.std(0))
+    return x
 
-# load valid x and y
-valid_data = loadmat('train_01_06/train_subject02.mat', squeeze_me=True)
-valid_x = valid_data['X']
-valid_y = valid_data['y']
+train_subjects = range(1,3) # load 1-15 and use 16 to validate
+train_x = []
+train_y = []
 
-# unroll arrays
-train_x = train_x.reshape(train_x.shape[0], train_x.shape[1] * train_x.shape[2])
-valid_x = valid_x.reshape(valid_x.shape[0], valid_x.shape[1] * valid_x.shape[2])
+for subject in train_subjects:
+    filename = 'train_01_16/train_subject%02d.mat' % subject
+    print 'Loading', filename
+    data = loadmat(filename, squeeze_me=True)
+    subject_x = data['X']
+    subject_y = data['y']
 
-# normalize
-train_x -= train_x.mean(0)
-train_x = np.nan_to_num(train_x / train_x.std(0))
+    subject_x = create_features(subject_x)
 
-valid_x -= valid_x.mean(0)
-valid_x = np.nan_to_num(valid_x / valid_x.std(0))
+    train_x.append(subject_x)
+    train_y.append(subject_y)
 
-# fit logistic regression
+train_x = np.vstack(train_x)
+train_y = np.concatenate(train_y)
+
+data = loadmat('train_01_16/train_subject16.mat',squeeze_me=True)
+valid_x = data['X']
+valid_y = data['y']
+
+valid_x = create_features(valid_x)
+
 clf = LogisticRegression(random_state=0)
 clf.fit(train_x, train_y)
 
-diff = clf.predict(valid_x)[0:2] - valid_y[0:2]
-print diff
-print (len(valid_y) - np.count_nonzero(diff)) / float(len(valid_y))
+predict_y = clf.predict(valid_x)
+
+diff = valid_y - predict_y
+print (len(valid_y) - np.count_nonzero(diff)) / len(valid_y)
