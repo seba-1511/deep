b7dcc1f2c13b88d7d53ef6349cd84c372a37983d
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-05-06 13:50:21 -0700

    Restricted time series interval

--- test.py
+++ test.py
@@ -6,15 +6,22 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from scipy.io import loadmat
 
-def create_features(x):
+def create_features(x, tmin, tmax, sfreq, tmin_original):
+    beginning = np.round((tmin - tmin_original)*sfreq).astype(np.int)
+    end       = np.round((tmax - tmin_original)*sfreq).astype(np.int)
+    x = x[:,:,beginning:end].copy()
+
     x = x.reshape(x.shape[0], x.shape[1] * x.shape[2])
     x -= x.mean(0)
     x = np.nan_to_num(x / x.std(0))
     return x
 
-train_subjects = range(1,3) # load 1-15 and use 16 to validate
+train_subjects = range(1,12) # load 1-15 and use 16 to validate
 train_x = []
 train_y = []
+
+tmin = 0.0
+tmax = 0.500
 
 for subject in train_subjects:
     filename = 'train_01_16/train_subject%02d.mat' % subject
@@ -23,7 +30,9 @@ for subject in train_subjects:
     subject_x = data['X']
     subject_y = data['y']
 
-    subject_x = create_features(subject_x)
+    sfreq = data['sfreq']
+    tmin_original = data['tmin']
+    subject_x = create_features(subject_x, tmin, tmax, sfreq, tmin_original)
 
     train_x.append(subject_x)
     train_y.append(subject_y)
@@ -34,8 +43,10 @@ train_y = np.concatenate(train_y)
 data = loadmat('train_01_16/train_subject16.mat',squeeze_me=True)
 valid_x = data['X']
 valid_y = data['y']
+tmin_original = data['tmin']
+sfreq = data['sfreq']
 
-valid_x = create_features(valid_x)
+valid_x = create_features(valid_x, tmin, tmax, sfreq, tmin_original)
 
 clf = LogisticRegression(random_state=0)
 clf.fit(train_x, train_y)
@@ -43,4 +54,4 @@ clf.fit(train_x, train_y)
 predict_y = clf.predict(valid_x)
 
 diff = valid_y - predict_y
-print (len(valid_y) - np.count_nonzero(diff)) / len(valid_y)
+print (len(valid_y) - np.count_nonzero(diff)) / float(len(valid_y))
