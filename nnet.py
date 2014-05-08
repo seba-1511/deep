3888c7104c4bbba64471956f2d23575ee413a4b6
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-05-08 09:29:48 -0700

    Built pybrain neural net without build shortcut

--- nnet.py
+++ nnet.py
@@ -1,71 +1,29 @@
-import sys
-sys.path.insert(0, "/usr/local/lib/python2.7/site-packages")
-import numpy as np
+import load_data
 
-from pybrain.tools.shortcuts import buildNetwork
-from pybrain.datasets import SupervisedDataSet
-from pybrain.supervised.trainers import BackpropTrainer
-from pybrain.structure import TanhLayer
+from pybrain.structure import FeedForwardNetwork
+from pybrain.structure import LinearLayer
+from pybrain.structure import SigmoidLayer
+from pybrain.structure import FullConnection
 
-from scipy.io import loadmat
+#train_x, train_y = load_data.load_data(range(1,2))
+#valid_x, valid_y = load_data.load_data(range(16,17))
+#test_x , test_y  = load_data.load_data(range(1,2), test=True)
 
-def create_features(x, tmin, tmax, sfreq, tmin_orginal=-0.5):
-    beginning = np.round((tmin-tmin_orginal)*sfreq).astype(np.int)
-    end       = np.round((tmax-tmin_orginal)*sfreq).astype(np.int)
-    x = x[:,:,beginning:end].copy()
-    x = x.reshape(x.shape[0], x.shape[1] * x.shape[2])
-    x -= x.mean(0)
-    x = np.nan_to_num(x / x.std(0))
-    return x
+n = FeedForwardNetwork()
+inLayer = LinearLayer(2)
+hiddenLayer = SigmoidLayer(3)
+outLayer = LinearLayer(1)
 
-train_subjects = range(1,2)
-train_x = []
-train_y = []
+n.addInputModule(inLayer)
+n.addModule(hiddenLayer)
+n.addOutputModule(outLayer)
 
-tmin = 0.0
-tmax = 0.500
+in_to_hidden = FullConnection(inLayer, hiddenLayer)
+hidden_to_out = FullConnection(hiddenLayer, outLayer)
 
-for subject in train_subjects:
-    filename = 'train_01_16/train_subject%02d.mat' % subject
-    print 'Loading', filename
-    data = loadmat(filename, squeeze_me=True)
-    subject_x = data['X']
-    subject_y = data['y']
-    tmin_original = data['tmin']
-    sfreq = data['sfreq']
-    subject_x = create_features(subject_x, tmin, tmax, sfreq)
-    train_x.append(subject_x)
-    train_y.append(subject_y)
+n.addConnection(in_to_hidden)
+n.addConnection(hidden_to_out)
 
-train_x = np.vstack(train_x)
-train_y = np.concatenate(train_y)
+n.sortModules()
 
-net = buildNetwork(38250,3,1, bias=True, hiddenclass=TanhLayer)
-ds = SupervisedDataSet(38250,1)
-
-print "Loading training set:", train_x.shape
-for inp, target in zip(train_x, train_y):
-    ds.addSample(inp, target)
-
-trainer = BackpropTrainer(net, ds)
-
-print trainer.train()
-print trainer.testOnData()
-
-data = loadmat('train_01_16/train_subject16.mat')
-valid_x = data['X']
-valid_y = data['y']
-tmin_original = data['tmin']
-sfreq = data['sfreq']
-
-valid_x = create_features(valid_x, tmin, tmax, sfreq, tmin_original)
-valid_x = np.vstack(valid_x)
-
-predict_y = []
-
-valid_ds = SupervisedDataSet(38250,1)
-print "Loading validation set:", valid_x.shape
-for inp, target in zip(valid_x, valid_y):
-    valid_ds.addSample(inp, target)
-
-print trainer.testOnData(valid_ds)
+print n
