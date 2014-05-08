709ec42989922d1959276d97849bf9fe1c24998d
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-05-08 09:50:32 -0700

    Classification with Feed-Forward Neural Networks example code

--- nnet.py
+++ nnet.py
@@ -1,29 +1,74 @@
 import load_data
 
-from pybrain.structure import FeedForwardNetwork
-from pybrain.structure import LinearLayer
-from pybrain.structure import SigmoidLayer
-from pybrain.structure import FullConnection
+# classification with fnn
+from pybrain.datasets import ClassificationDataSet
+from pybrain.utilities import percentError
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.structure.modules import SoftmaxLayer
+
+# graphics
+from pylab import ion, ioff, figure, draw, contourf, clf, show, hold, plot
+from scipy import diag, arange, meshgrid, where
+from numpy.random import multivariate_normal
 
 #train_x, train_y = load_data.load_data(range(1,2))
 #valid_x, valid_y = load_data.load_data(range(16,17))
 #test_x , test_y  = load_data.load_data(range(1,2), test=True)
 
-n = FeedForwardNetwork()
-inLayer = LinearLayer(2)
-hiddenLayer = SigmoidLayer(3)
-outLayer = LinearLayer(1)
+means = [(-1,0),(2,4),(3,1)]
+cov = [diag([1,1]),diag([0.5,1.2]),diag([1.5,0.7])]
+alldata = ClassificationDataSet(2,1,nb_classes=3)
+for n in xrange(400):
+        for klass in range(3):
+                input = multivariate_normal(means[klass], cov[klass])
+                alldata.addSample(input, [klass])
 
-n.addInputModule(inLayer)
-n.addModule(hiddenLayer)
-n.addOutputModule(outLayer)
+tstdata, trndata = alldata.splitWithProportion(0.25)
 
-in_to_hidden = FullConnection(inLayer, hiddenLayer)
-hidden_to_out = FullConnection(hiddenLayer, outLayer)
+trndata._convertToOneOfMany()
+tstdata._convertToOneOfMany()
 
-n.addConnection(in_to_hidden)
-n.addConnection(hidden_to_out)
+print "Number of training patters: ", len(trndata)
+print "Input and output dimensions: ", trndata.indim, trndata.outdim
+print "First sample (input, target, class):"
+print trndata['input'][0], trndata['target'][0],trndata['class'][0]
 
-n.sortModules()
+fnn = buildNetwork(trndata.indim, 5, trndata.outdim, outclass=SoftmaxLayer)
+trainer = BackpropTrainer(fnn, dataset=trndata, momentum=0.1, verbose=True, weightdecay=0.01)
 
-print n
+ticks = arange(-3.,6.,0.2)
+X,Y = meshgrid(ticks, ticks)
+griddata = ClassificationDataSet(2,1,nb_classes=3)
+for i in xrange(X.size):
+        griddata.addSample([X.ravel()[i],Y.ravel()[i]], [0])
+griddata._convertToOneOfMany()
+
+for i in range(20):
+    trainer.trainEpochs(1)
+    trnresult = percentError(trainer.testOnClassData(),
+                            trndata['class'])
+    tstresult = percentError(trainer.testOnClassData(
+        dataset=tstdata), tstdata['class'])
+    print "epoch: %4d" % trainer.totalepochs, \
+        " train error: %5.2f%%" % trnresult, \
+        " test error: %5.2f%%" % tstresult
+
+    out = fnn.activateOnDataset(griddata)
+    out = out.argmax(axis=1)
+    out = out.reshape(X.shape)
+
+    figure(1)
+    ioff()
+    clf()
+    hold(True)
+    for c in [0,1,2]:
+        here, _ = where(tstdata['class']==c)
+        plot(tstdata['input'][here,0],tstdata['input'][here,1],'o')
+        if out.max() != out.min():
+            contourf(X,Y,out)
+    ion()
+    draw()
+
+ioff()
+show()
