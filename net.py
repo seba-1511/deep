65934c32b14c1fb7480981b9c930aa7b262d0e29
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-05-19 16:28:39 -0700

    Testing svm on individual sensors

--- net.py
+++ net.py
@@ -28,22 +28,17 @@ def load_data(subject_range, start=0, end=375):
 
     return train_x, train_y
 
-for length in range(25,376,25):
-    for start in range(25,375,25):
+if __name__ == "__main__":
 
-        if start+length <= 375:
+    for sensor in range(306):
 
+        train_x, train_y = load_data(range(1,9),125, 225)
+        valid_x, valid_y = load_data(range(9,17), 125, 225)
 
-            train_x, train_y = load_data(range(1,2),start, start+length)
-            valid_x, valid_y = load_data(range(9,10), start, start+length)
+        train_x = train_x[:,sensor,:]
+        valid_x = valid_x[:,sensor,:]
 
-            train_x = train_x.reshape(train_x.shape[0], train_x.shape[1]*train_x.shape[2])
-            valid_x = valid_x.reshape(valid_x.shape[0], valid_x.shape[1]*valid_x.shape[2])
+        linear_svm = svm.LinearSVC()
+        linear_svm.fit(train_x, train_y)
 
-            linear_svm = svm.LinearSVC()
-            linear_svm.fit(train_x, train_y)
-
-            svc = svm.SVC()
-            svc.fit(train_x, train_y)
-
-            print "Start:", start, "End:", start+length, "LinearSVC:", linear_svm.score(valid_x, valid_y), "SVC:", svc.score(valid_x, valid_y)
+        print "Sensor:", sensor, "LinearSVC:", linear_svm.score(valid_x, valid_y)
