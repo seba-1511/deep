5f9663abd7c109c645a80978323f37be9a823337
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-05-21 11:15:10 -0700

    Stacked svm submission

--- net.py
+++ net.py
@@ -13,15 +13,15 @@ def load_data(subject_range, start=0, end=375, test=False):
     for subject in subject_range:
 
         if test:
-            filename = 'train_01_16/train_subject%02d.mat' % subject
-            data = loadmat(filename, squeeze_me=True)
-            x = data['X']
-            y = data['y']
-        else:
             filename = 'test_17_23/test_subject%02d.mat' % subject
             data = loadmat(filename, squeeze_me=True)
             x = data['X']
             y = data['Id']
+        else:
+            filename = 'train_01_16/train_subject%02d.mat' % subject
+            data = loadmat(filename, squeeze_me=True)
+            x = data['X']
+            y = data['y']
         x = zscore(x)
         x = x[:,:,start:end].copy()
 
@@ -37,57 +37,54 @@ if __name__ == "__main__":
 
     stacked_svms = []
 
-    for i in range(1,10):
+    for i in range(1,17):
 
         print "Training subject #", i
 
-        train_x, train_y = load_data(range(i,i+1),125,225)
+        train_x, train_y = load_data(range(i,i+1),125,225, test=False)
         train_x = train_x.reshape(train_x.shape[0],train_x.shape[1]*train_x.shape[2])
 
-
+        """
         # make smaller for testing
         train_x = train_x[:10,:].copy()
         train_y = train_y[:10].copy()
-
+        """
 
         linear_svm = svm.SVC(probability=True, kernel='linear')
         linear_svm.fit(train_x, train_y)
 
         stacked_svms.append(linear_svm)
 
-
     total_correct = 0
     total_trials = 0
 
+    # generating test file
 
-    for i in range(10,14):
+    filename_submission = "submission.csv"
+    print "Creating submission file", filename_submission
+    f = open(filename_submission, "w")
+    print >> f, "Id,Prediction"
 
-    
 
+    for i in range(17,24):
 
-
-
-        print "Validating subject #", i
-
-        valid_x, valid_y = load_data(range(i,i+1),125,225)
-        valid_x = valid_x.reshape(valid_x.shape[0],valid_x.shape[1]*valid_x.shape[2])
+        test_x, test_y = load_data(range(i,i+1),125,225, test=True)
+        test_x = test_x.reshape(test_x.shape[0],test_x.shape[1]*test_x.shape[2])
 
         """
         #make smaller for testing
-        valid_x = valid_x[:50,:].copy()
-        valid_y = valid_y[:50].copy()
+        test_x = test_x[:50,:].copy()
+        test_y = test_y[:50].copy()
         """
 
-        correct = 0
-
-        for i in range(len(valid_x)):
+        for i in range(len(test_x)):
 
             print "Trial #", i
 
             probs = []
 
             for j in range(len(stacked_svms)):
-                probs.append(stacked_svms[j].predict_proba(valid_x[i])[0])
+                probs.append(stacked_svms[j].predict_proba(test_x[i])[0])
 
             # stack probabilities of classes
             # rows = different svms
@@ -95,25 +92,9 @@ if __name__ == "__main__":
             # take max col as prediction (link to how to get max index)
             # http://stackoverflow.com/questions/3584243/python-get-the-position-of-the-biggest-item-in-a-numpy-array
 
-            print "Label", valid_y[i]
             probs = np.vstack(probs)
-            print probs
             prediction = np.unravel_index(probs.argmax(), probs.shape)[1]
-            print prediction
 
-            if prediction == valid_y[i]:
+            print >> f, str(test_y[i]) + "," + str(prediction)
 
-                print "Correct!"
-
-                correct += 1
-
-        total_correct += correct
-        total_trials += len(valid_y)
-
-        print correct
-        print len(valid_y)
-        print float(correct) / len(valid_y)
-
-    print total_correct
-    print total_trials
-    print float(total_correct) / total_trials
+    f.close
