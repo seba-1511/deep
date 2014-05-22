3d84722db4a1f425a737f8cda79d3044bacc8166
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-05-22 16:10:02 -0700

    Graph best sensors and performance by dropping worst sensors

--- net.py
+++ net.py
@@ -4,6 +4,7 @@ from scipy.io import loadmat
 from scipy.stats.mstats import zscore
 import numpy as np
 from sklearn import svm
+from sklearn.cross_validation import cross_val_score
 
 def load_data(subject_range, start=0, end=375, test=False):
 
@@ -14,14 +15,17 @@ def load_data(subject_range, start=0, end=375, test=False):
 
         if test:
             filename = 'test_17_23/test_subject%02d.mat' % subject
-            data = loadmat(filename, squeeze_me=True)
-            x = data['X']
-            y = data['Id']
         else:
             filename = 'train_01_16/train_subject%02d.mat' % subject
-            data = loadmat(filename, squeeze_me=True)
-            x = data['X']
+
+        data = loadmat(filename, squeeze_me=True)
+
+        if test:
+            y = data['Id']
+        else:
             y = data['y']
+
+        x = data['X']
         x = zscore(x)
         x = x[:,:,start:end].copy()
 
@@ -35,66 +39,30 @@ def load_data(subject_range, start=0, end=375, test=False):
 
 if __name__ == "__main__":
 
-    stacked_svms = []
+    # load data
+    train_x, train_y = load_data(range(1,2))
 
-    for i in range(1,17):
+    # create svm and score arrays
+    channel_svm = svm.LinearSVC()
+    score_channel = np.zeros(train_x.shape[1])
+    score_channel_sums = np.zeros(train_x.shape[1])
 
-        print "Training subject #", i
+    # cross-validate each sensor
+    for channel in range(train_x.shape[1]):
+        scores = cross_val_score(channel_svm, train_x[:,channel], train_y, cv=2, scoring='accuracy')
+        score_channel[channel] = scores.mean()
+        print "Channel #", channel, "Score:", score_channel[channel]
 
-        train_x, train_y = load_data(range(i,i+1),125,225, test=False)
-        train_x = train_x.reshape(train_x.shape[0],train_x.shape[1]*train_x.shape[2])
+    print np.sort(score_channel)
 
-        """
-        # make smaller for testing
-        train_x = train_x[:10,:].copy()
-        train_y = train_y[:10].copy()
-        """
+    # cross validate dropping worst sensor
+    for channel in range(train_x.shape[1]):
+        x = train_x[:,channel:,:]
+        x = x.reshape(x.shape[0], x.shape[1]*x.shape[2])
+        scores = cross_val_score(channel_svm, x, train_y, cv=2, scoring='accuracy')
+        score_channel_sums[channel] = scores.mean()
+        print "Train_x", x.shape, "Score:", score_channel_sums[channel]
 
-        linear_svm = svm.SVC(probability=True, kernel='linear')
-        linear_svm.fit(train_x, train_y)
-
-        stacked_svms.append(linear_svm)
-
-    total_correct = 0
-    total_trials = 0
-
-    # generating test file
-
-    filename_submission = "submission.csv"
-    print "Creating submission file", filename_submission
-    f = open(filename_submission, "w")
-    print >> f, "Id,Prediction"
-
-
-    for i in range(17,24):
-
-        test_x, test_y = load_data(range(i,i+1),125,225, test=True)
-        test_x = test_x.reshape(test_x.shape[0],test_x.shape[1]*test_x.shape[2])
-
-        """
-        #make smaller for testing
-        test_x = test_x[:50,:].copy()
-        test_y = test_y[:50].copy()
-        """
-
-        for i in range(len(test_x)):
-
-            print "Trial #", i
-
-            probs = []
-
-            for j in range(len(stacked_svms)):
-                probs.append(stacked_svms[j].predict_proba(test_x[i])[0])
-
-            # stack probabilities of classes
-            # rows = different svms
-            # cols = probability of col # class
-            # take max col as prediction (link to how to get max index)
-            # http://stackoverflow.com/questions/3584243/python-get-the-position-of-the-biggest-item-in-a-numpy-array
-
-            probs = np.vstack(probs)
-            prediction = np.unravel_index(probs.argmax(), probs.shape)[1]
-
-            print >> f, str(test_y[i]) + "," + str(prediction)
-
-    f.close
+    plt.plot(score_channel)
+    plt.plot(score_channel_sums)
+    plt.show()
