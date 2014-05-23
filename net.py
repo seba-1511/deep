9aca51d218c96abbd1d82509d777794dc2940f3e
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-05-22 17:16:04 -0700

    Fixed sensor selection logic

--- net.py
+++ net.py
@@ -5,6 +5,7 @@ from scipy.stats.mstats import zscore
 import numpy as np
 from sklearn import svm
 from sklearn.cross_validation import cross_val_score
+from matplotlib import pyplot as plt
 
 def load_data(subject_range, start=0, end=375, test=False):
 
@@ -40,7 +41,8 @@ def load_data(subject_range, start=0, end=375, test=False):
 if __name__ == "__main__":
 
     # load data
-    train_x, train_y = load_data(range(1,2))
+    train_x, train_y = load_data(range(1,2),125,225)
+    valid_x, valid_y = load_data(range(2,3),125,225)
 
     # create svm and score arrays
     channel_svm = svm.LinearSVC()
@@ -51,18 +53,27 @@ if __name__ == "__main__":
     for channel in range(train_x.shape[1]):
         scores = cross_val_score(channel_svm, train_x[:,channel], train_y, cv=2, scoring='accuracy')
         score_channel[channel] = scores.mean()
-        print "Channel #", channel, "Score:", score_channel[channel]
+        print "Channel #", channel+1, "Score:", score_channel[channel]
 
-    print np.sort(score_channel)
+    score_index = np.argsort(score_channel)
+
+    print
+    print "Sensors from least to greatest:"
+    print
+    for i in range(len(score_channel)):
+        print "Channel #", score_index[i]+1, "Score:", score_channel[score_index[i]]
 
     # cross validate dropping worst sensor
-    for channel in range(train_x.shape[1]):
-        x = train_x[:,channel:,:]
+    for channel in range(0,train_x.shape[1]+1,5):
+
+        print score_channel[score_index[channel:]]
+
+        x = train_x[:,score_index[channel:],:]
         x = x.reshape(x.shape[0], x.shape[1]*x.shape[2])
         scores = cross_val_score(channel_svm, x, train_y, cv=2, scoring='accuracy')
         score_channel_sums[channel] = scores.mean()
         print "Train_x", x.shape, "Score:", score_channel_sums[channel]
 
-    plt.plot(score_channel)
-    plt.plot(score_channel_sums)
+    plt.plot(np.sort(score_channel))
+    plt.plot(np.sort(score_channel_sums))
     plt.show()
