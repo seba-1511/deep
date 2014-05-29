754e7c77b9d43899c88c306d045b7d65cd049fef
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-05-29 09:38:55 -0700

    Sklearn ICA example

--- net.py
+++ net.py
@@ -3,84 +3,73 @@ sys.path.insert(0, "/usr/local/lib/python2.7/site-packages")
 from scipy.io import loadmat
 from scipy.stats.mstats import zscore
 import numpy as np
-from sklearn import svm, neural_network
-from sklearn.cross_validation import cross_val_score
-from matplotlib import pyplot as plt
+from sklearn import svm, decomposition
 
 def load_data(subject_range, start=0, end=375, test=False):
 
-    train_x = []
-    train_y = []
+    # list of all subjects
+    x = []
+    y = []
 
+    # load subject data
     for subject in subject_range:
 
+        # open train/test file
         if test:
             filename = 'test_17_23/test_subject%02d.mat' % subject
         else:
             filename = 'train_01_16/train_subject%02d.mat' % subject
 
+        # load
         data = loadmat(filename, squeeze_me=True)
 
+        # test = id/train = labels
         if test:
-            y = data['Id']
+            subject_y = data['Id']
         else:
-            y = data['y']
+            subject_y = data['y']
 
-        x = data['X']
-        x = zscore(x)
-        x = x[:,:,start:end].copy()
+        # z score and resize
+        subject_x = data['X']
+        subject_x = zscore(subject_x)
+        subject_x = subject_x[:,:,start:end].copy()
 
-        train_x.append(x)
-        train_y.append(y)
+        # append to subject list
+        x.append(subject_x)
+        y.append(subject_y)
 
-    train_x = np.vstack(train_x)
-    train_y = np.concatenate(train_y)
+    # make numpy arrays
+    x = np.vstack(x)
+    y = np.concatenate(y)
 
-    return train_x, train_y
+    return x, y
 
 if __name__ == "__main__":
 
-    # load data
-    train_x, train_y = load_data(range(1,2))
-    test_x, test_y = load_data(range(17,18), test=True)
+    """
+    # load train/valid data
+    train_x, train_y = load_data(range(3,5))
+    valid_x, valid_y = load_data(range(12,14))
 
     # concatenate sensors
     train_x = train_x.reshape(train_x.shape[0], train_x.shape[1]*train_x.shape[2])
-    test_x = test_x.reshape(test_x.shape[0], test_x.shape[1]*test_x.shape[2])
+    valid_x = valid_x.reshape(valid_x.shape[0], valid_x.shape[1]*valid_x.shape[2])
+    """
 
-    # shorten for testing
-    train_x = train_x[:10].copy()
-    test_x = test_x[:10].copy()
+    # generate sample data
+    time = np.linspace(0,10,2000)
+    s1 = np.sin(2*time)
+    s2 = np.sign(np.sin(3*time))
+    S = np.c_[s1,s2]
+    S += 0.2*np.random.normal(size=S.shape)
+    S /= S.std(axis=0)
 
+    # mix data
+    A = np.array([[1,1],[0.5,2]])
+    X = np.dot(S, A.T)
 
-    # create export file
-    f = open('train_transduction.dat','w')
-
-    # write labeled data
-    for trial in range(len(train_x)):
-
-        print "Writing labeled trial #", trial, "/", len(train_x)
-
-        if train_y[trial] == 1:
-            line = "1"
-        else:
-            line = "-1"
-
-        for feature in range(len(train_x[trial])):
-            line += " " + str(feature+1) + ":" + str(train_x[trial][feature])
-
-        print >> f, line
-
-    # write unlabeled data
-    for trial in range(len(test_x)):
-
-        print "Writing unlabeled trial #", trial, "/", len(train_x)
-
-        line = "0"
-
-        for feature in range(len(test_x[trial])):
-            line += " " + str(feature+1) + ":" + str(train_x[trial][feature])
-
-        print >> f, line
-
-    f.close()
+    # compute ICA
+    ica = decomposition.FastICA()
+    S_ = ica.fit_transform(X)
+    A_ = ica.mixing_.T
+    np.allclose(X, np.dot(S_, A_) + ica.mean_)
