fc6f488b2b8ba79298811971e0fd905026995a9f
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-05-30 11:49:26 -0700

    Parse digits csv file

--- theano_practice.py
+++ theano_practice.py
@@ -1,41 +1,22 @@
 import sys
 sys.path.insert(0, "/usr/local/lib/python2.7/site-packages")
 
-import numpy
+import numpy as np
 import theano
+import csv
 
-rng = numpy.random
 
-N = 400
-feats = 784
-D = (rng.randn(N, feats), rng.randint(size=N, low=0, high=2))
-training_steps = 1000
+# open file and discard header
+f = open('kaggle_digits/train.csv', 'r')
+f.readline()
 
-# declare symbolic variables
-x = theano.tensor.matrix('x')
-y = theano.tensor.vector('y')
-w = theano.shared(rng.randn(feats), name='w')
-b = theano.shared(0., name='w')
-print 'Initial model:'
-print w.get_value(), b.get_value()
+# parse into numpy array
+d = [line.split(',') for line in f.read().split()]
+d = np.array(d)
 
-# construct expression graph
-p_1 = 1 / (1 + theano.tensor.exp(-theano.tensor.dot(x,w)-b))
-prediction = p_1 > 0.5
-xent = -y * theano.tensor.log(p_1) - (1-y) * theano.tensor.log(1-p_1)
-cost = xent.mean() + 0.01 * (w**2).sum()
-gw, gb = theano.tensor.grad(cost, [w,b])
+# separate labels and examples
+y = d[:,0]
+x = d[:,1:]
 
-# compile
-train = theano.function(inputs=[x,y], outputs=[prediction,xent],
-                        updates=(((w,w-0.1*gw),(b,b-0.1*gb))))
-predict = theano.function(inputs=[x], outputs=prediction)
-
-# train
-for i in range(training_steps):
-    pred, err = train(D[0], D[1])
-
-print "final model:"
-print w.get_value(), b.get_value()
-print "target values for D:", D[1]
-print "prediction on D:", predict(D[0])
+print y.shape
+print x.shape
