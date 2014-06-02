920ffc84b32ca794f85e0b2c8247395dc3ced87a
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-06-02 16:47:04 -0700

    Theano logistic regression class

--- theano_practice.py
+++ theano_practice.py
@@ -123,6 +123,38 @@ def theano_load_data(dataset):
             (test_set_x, test_set_y)]
     return rval
 
+class LogisticRegression(object):
+
+    def __init__(self, input, n_in, n_out):
+
+        self.W = theano.shared(value=numpy.zeros((n_in, n_out),
+                                                 dtype=theano.config.floatX),
+                               name='W', borrow=True)
+
+        self.b = theano.shared(value=numpy.zeros((n_out,),
+                                                 dtype=theano.config.floatX),
+                               name='b', borrow=True)
+
+        self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
+
+        self.y_pred = T.argmax(self.p_y_given_x, axis=1)
+
+        self.params = [self.W, self.b]
+
+    def negative_log_likelihood(self, y):
+
+        return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]),y])
+
+    def errors(self, y):
+
+        if y.ndim != self.y_pred.ndim:
+            raise TypeError('y should have the same shape as self.y_pred',
+                ('y', target.type, 'y_pred', self.y_pred.type))
+
+        if y.dtype.startswith('int'):
+            return T.mean(T.neq(self.y_pred, y))
+        else:
+            raise NotImplementedError()
 
 if __name__ == '__main__':
 
@@ -143,18 +175,3 @@ if __name__ == '__main__':
     train_set_x, train_set_y = datasets[0]
     valid_set_x, valid_set_y = datasets[1]
     test_set_x, test_set_y   = datasets[2]
-
-    print theano_train_set_x.get_value(borrow=True).shape
-    print theano_train_set_y.ndim
-    print theano_valid_set_x.get_value(borrow=True).shape
-    print theano_valid_set_y.ndim
-    print theano_test_set_x.get_value(borrow=True).shape
-    print theano_test_set_y.ndim
-    print
-    print
-    print train_set_x.get_value(borrow=True).shape
-    print train_set_y.ndim
-    print valid_set_x.get_value(borrow=True).shape
-    print valid_set_y.ndim
-    print test_set_x.get_value(borrow=True).shape
-    print test_set_y.ndim
