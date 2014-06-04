55ff0660277fd3ead35fc75023914fd32fcc9863
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-06-04 08:18:01 -0700

    Fixed nan output error

--- theano_practice.py
+++ theano_practice.py
@@ -34,8 +34,8 @@ def load_data(dataset):
     data_y = data[:,0]
 
     # split data parameters
-    end_of_train = data.shape[0] * .5
-    end_of_valid = data.shape[0] * .25
+    end_of_train = data.shape[0] * .8
+    end_of_valid = data.shape[0] * .9
 
     # split data
     train_set_x = data_x[:end_of_train]
@@ -408,8 +408,9 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
 
     train_set_x, train_set_y = datasets[0]
     valid_set_x, valid_set_y = datasets[1]
-    test_set_x , test_set_y  = datasets[2]
+    test_set_x, test_set_y = datasets[2]
 
+    # compute number of minibatches for training, validation and testing
     n_train_batches = train_set_x.get_value(borrow=True).shape[0]
     n_valid_batches = valid_set_x.get_value(borrow=True).shape[0]
     n_test_batches = test_set_x.get_value(borrow=True).shape[0]
@@ -417,33 +418,55 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
     n_valid_batches /= batch_size
     n_test_batches /= batch_size
 
-    index = T.lscalar()
-    x = T.matrix('x')
-    y = T.ivector('y')
+    # allocate symbolic variables for the data
+    index = T.lscalar()  # index to a [mini]batch
+    x = T.matrix('x')   # the data is presented as rasterized images
+    y = T.ivector('y')  # the labels are presented as 1D vector of
+                        # [int] labels
 
-    ishape = (28,28)
+    ishape = (28, 28)  # this is the size of MNIST images
 
-    print '... building model'
+    ######################
+    # BUILD ACTUAL MODEL #
+    ######################
+    print '... building the model'
 
+    # Reshape matrix of rasterized images of shape (batch_size,28*28)
+    # to a 4D tensor, compatible with our LeNetConvPoolLayer
     layer0_input = x.reshape((batch_size, 1, 28, 28))
 
+    # Construct the first convolutional pooling layer:
+    # filtering reduces the image size to (28-5+1,28-5+1)=(24,24)
+    # maxpooling reduces this further to (24/2,24/2) = (12,12)
+    # 4D output tensor is thus of shape (batch_size,nkerns[0],12,12)
     layer0 = LeNetConvPoolLayer(rng, input=layer0_input,
-                                image_shape=(batch_size, 1, 28, 28),
-                                filter_shape=(nkerns[0], 1, 5, 5), poolsize=(2, 2))
+            image_shape=(batch_size, 1, 28, 28),
+            filter_shape=(nkerns[0], 1, 5, 5), poolsize=(2, 2))
 
+    # Construct the second convolutional pooling layer
+    # filtering reduces the image size to (12-5+1,12-5+1)=(8,8)
+    # maxpooling reduces this further to (8/2,8/2) = (4,4)
+    # 4D output tensor is thus of shape (nkerns[0],nkerns[1],4,4)
     layer1 = LeNetConvPoolLayer(rng, input=layer0.output,
-                                image_shape=(batch_size, nkerns[0], 12, 12),
-                                filter_shape=(nkerns[1], nkerns[0], 5, 5), poolsize=(2, 2))
+            image_shape=(batch_size, nkerns[0], 12, 12),
+            filter_shape=(nkerns[1], nkerns[0], 5, 5), poolsize=(2, 2))
 
+    # the HiddenLayer being fully-connected, it operates on 2D matrices of
+    # shape (batch_size,num_pixels) (i.e matrix of rasterized images).
+    # This will generate a matrix of shape (20,32*4*4) = (20,512)
     layer2_input = layer1.output.flatten(2)
 
-    layer2 = HiddenLayer(rng, input=layer2_input, n_in=nkerns[1]*4*4,
+    # construct a fully-connected sigmoidal layer
+    layer2 = HiddenLayer(rng, input=layer2_input, n_in=nkerns[1] * 4 * 4,
                          n_out=500, activation=T.tanh)
 
+    # classify the values of the fully-connected sigmoidal layer
     layer3 = LogisticRegression(input=layer2.output, n_in=500, n_out=10)
 
+    # the cost we minimize during training is the NLL of the model
     cost = layer3.negative_log_likelihood(y)
 
+    # create a function to compute the mistakes that are made by the model
     test_model = theano.function([index], layer3.errors(y),
              givens={
                 x: test_set_x[index * batch_size: (index + 1) * batch_size],
@@ -454,10 +477,17 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
                 x: valid_set_x[index * batch_size: (index + 1) * batch_size],
                 y: valid_set_y[index * batch_size: (index + 1) * batch_size]})
 
+    # create a list of all model parameters to be fit by gradient descent
     params = layer3.params + layer2.params + layer1.params + layer0.params
 
+    # create a list of gradients for all model parameters
     grads = T.grad(cost, params)
 
+    # train_model is a function that updates the model parameters by
+    # SGD Since this model has many parameters, it would be tedious to
+    # manually create an update rule for each model parameter. We thus
+    # create the updates list by automatically looping over all
+    # (params[i],grads[i]) pairs.
     updates = []
     for param_i, grad_i in zip(params, grads):
         updates.append((param_i, param_i - learning_rate * grad_i))
@@ -467,12 +497,21 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
             x: train_set_x[index * batch_size: (index + 1) * batch_size],
             y: train_set_y[index * batch_size: (index + 1) * batch_size]})
 
-    print '... training model'
-
-    patience = 10000
-    patience_increase = 2
-    improvement_threshold = 0.995
+    ###############
+    # TRAIN MODEL #
+    ###############
+    print '... training'
+    # early-stopping parameters
+    patience = 10000  # look as this many examples regardless
+    patience_increase = 2  # wait this much longer when a new best is
+                           # found
+    improvement_threshold = 0.995  # a relative improvement of this much is
+                                   # considered significant
     validation_frequency = min(n_train_batches, patience / 2)
+                                  # go through this many
+                                  # minibatche before checking the network
+                                  # on the validation set; in this case we
+                                  # check every epoch
 
     best_params = None
     best_validation_loss = numpy.inf
