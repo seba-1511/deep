705253e5753594c204a53038f29a543d0b56c39a
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-06-11 10:16:11 -0700

    Neural network feedforward function and cost function

--- net.py
+++ net.py
@@ -2,44 +2,56 @@ import sys
 sys.path.insert(0, "/usr/local/lib/python2.7/site-packages")
 import numpy as np
 import matplotlib.pyplot as plt
+import cPickle
+import gzip
+
+def create_y_vector(y):
+    y_vector = np.zeros((10,1))
+    y_vector[y] = 1
+    return y_vector
 
 def sigmoid(z):
-
     return 1 / (1 + np.exp(-z))
 
-def cost(w, x, y):
+def feedforward(weights, a):
 
-    return np.sum([(np.dot(x,w) - y)**2 for x,y in zip(train_x, y)])
+    # for each layer
+    for w in weights:
+        a = sigmoid(np.dot(w, a))
+        # add 1 for bias
+        a = np.insert(a,0,1)
+    # remove 1 from final activation
+    return a[1:]
 
-def update(w, train_x, y):
+def cost(weights, set_x, set_y):
 
-    for x, y in zip(train_x, y):
-        w = w + 0.1*(y - sigmoid(np.dot(x, w)))*x
-    return w
+    # sum of the square difference of output and label vector
+    return np.sum([(feedforward(weights, x) - train_y)**2 for
+                   x, y in zip(set_x, set_y)])
 
 if __name__ == "__main__":
 
-    # seed and generate x and y
-    np.random.seed(7)
-    x = np.random.randn(10,2) + 2
-    y = np.random.randint(0,2,10)
+    # random seed
+    np.random.seed(1)
 
-    # generate weights
-    w = np.random.randn(3)
+    # load training data
+    f = gzip.open('data/mnist.pkl.gz', 'rb')
+    training_data, validation_data, test_data = cPickle.load(f)
 
-    # append ones to x
-    train_x  = np.column_stack((np.ones(10), x))
+    # take small subset for testing
+    train_x = training_data[0][:100]
+    train_y = training_data[1][:100]
 
-    for i in range(100):
-        w = update(w, train_x, y)
-        print cost(w, train_x, y)
+    # append 0's to x's and
+    # convert y labels to vectors
+    train_x = np.column_stack((np.ones(len(train_x)),train_x))
+    train_y = [create_y_vector(y) for y in train_y]
 
-    # compute line and plot
-    line = [-(w[0] + w[1] * i) / w[2]  for i in range(5)]
-    plt.plot(line)
+    # initialize network
+    size = [784, 30, 10]
+    weights = [np.random.randn(x,y+1) for x, y in zip(size[1:], size[:-1])]
 
-    # stack columns (since zip won't work) and plot
-    d = np.column_stack((x,y))
-    [plt.scatter(x1,x2, c=('r' if y == 1 else 'b')) for x1, x2, y in d]
-    plt.show()
+    # feedforward
+    print feedforward(weights, train_x[0])
 
+    print cost(weights, train_x, train_y)
