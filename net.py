74290212b28ef646ef1f5202804c79c18b8bf813
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-06-19 14:55:05 -0700

    initializing of neural network

--- net.py
+++ net.py
@@ -6,28 +6,16 @@ import cPickle
 import gzip
 
 def create_y_vector(y):
-    y_vector = np.zeros((10,1))
+    y_vector = np.zeros(10)
     y_vector[y] = 1
     return y_vector
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
-def feedforward(weights, a):
+def sigmoid_prime(z):
+    return sigmoid(z) * (1-sigmoid(z))
 
-    # for each layer
-    for w in weights:
-        a = sigmoid(np.dot(w, a))
-        # add 1 for bias
-        a = np.insert(a,0,1)
-    # remove 1 from final activation
-    return a[1:]
-
-def cost(weights, set_x, set_y):
-
-    # sum of the square difference of output and label vector
-    return np.sum([(feedforward(weights, x) - train_y)**2 for
-                   x, y in zip(set_x, set_y)])
 
 if __name__ == "__main__":
 
@@ -42,16 +30,10 @@ if __name__ == "__main__":
     train_x = training_data[0][:100]
     train_y = training_data[1][:100]
 
-    # append 0's to x's and
     # convert y labels to vectors
-    train_x = np.column_stack((np.ones(len(train_x)),train_x))
     train_y = [create_y_vector(y) for y in train_y]
 
     # initialize network
     size = [784, 30, 10]
-    weights = [np.random.randn(x,y+1) for x, y in zip(size[1:], size[:-1])]
-
-    # feedforward
-    print feedforward(weights, train_x[0])
-
-    print cost(weights, train_x, train_y)
+    weights = [np.random.randn(x,y) for x, y in zip(size[1:], size[:-1])]
+    biases = [np.random.randn]
