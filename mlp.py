96c36931ed2f7994297a84aa7eacda8e7aecf58f
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-07-20 14:18:22 -0700

    working MLP class constructed out of two sigmoid layers

--- mlp.py
+++ mlp.py
@@ -19,7 +19,7 @@ def sigmoid_prime(x):
 
 
 class SigmoidLayer():
-    """ a non-linear layer of a mlp """
+    """ a non-linear layer of a multi-layer perceptron """
 
     def __init__(self, input_size, output_size):
         """ initialize weights uniformly """
@@ -33,55 +33,91 @@ class SigmoidLayer():
             size=((output_size, input_size))
         )
 
-    def linear(self, input_x):
-        """ linear output """
+        self.activation_below = None
+        self.linear_activation = None
+        self.non_linear_activation = None
+        self.delta = None
 
-        return np.dot(self.weights, input_x) + self.bias
+    def fprop(self, activation_below):
 
-    def non_linear(self, linear):
-        """ non-linear output """
+        self.activation_below = activation_below
+        self.linear_activation = np.dot(self.weights, activation_below) + self.bias
+        self.non_linear_activation = sigmoid(self.linear_activation)
+        return self.non_linear_activation
 
-        return sigmoid(linear)
-
-    def update(self, input_x, label_y):
+    def bprop(self, error_above):
         """ update weights """
 
-        linear = self.linear(input_x)
-        non_linear = self.non_linear(linear)
+        self.delta = error_above * sigmoid_prime(self.linear_activation)
+        return np.dot(self.weights.T, self.delta)
 
-        delta = -(label_y - non_linear) * sigmoid_prime(linear)
+    def update(self):
 
-        self.weights -= 0.1 * np.dot(delta, input_x.T)
-        self.bias -= 0.1 * delta
+        self.weights -= 0.1 * np.dot(self.delta, self.activation_below.T)
+        self.bias -= 0.1 * self.delta
 
-    def score(self, valid_x, valid_y):
+
+class MLP():
+    """ a multi-layer perceptron """
+
+    def __init__(self, layers):
+        """ initialize mlp with a list of layers """
+
+        assert isinstance(layers, list)
+        assert all(isinstance(layer, SigmoidLayer) for layer in layers)
+        assert len(layers) >= 1
+
+        self.layers = layers
+
+    def fprop(self, input_x):
+        """ calculate activation of each layer """
+
+        rval = input_x
+        for layer in layers:
+             rval = layer.fprop(rval)
+        return rval
+
+    def bprop(self, input_x, label_y):
+        """ calculate gradient of each layer """
+
+        error = self.fprop(input_x) - label_y
+        for layer in self.layers[::-1]:
+            error = layer.bprop(error)
+
+    def update(self):
+        """ apply gradient update to each layer """
+
+        for layer in layers:
+            layer.update()
+
+    def train(self, train_x, train_y):
+
+        # TODO: move train to separate file
+
+        for x, y in zip(train_x, train_y):
+
+            self.bprop(x, y)
+            self.update()
 
         correct = 0.0
+        for x, y in zip(train_x, train_y):
 
-        for x, y in zip(valid_x, valid_y):
+            print np.argmax(self.fprop(x)), np.argmax(y)
 
-            linear = self.linear(x)
-            non_linear = self.non_linear(linear)
-
-            print non_linear
-
-            if np.argmax(non_linear) == np.argmax(y):
+            if np.argmax(self.fprop(x)) == np.argmax(y):
                 correct += 1.0
 
         return correct / len(train_x)
 
-    def predict(self, input_x):
-
-        linear = self.linear(input_x)
-        non_linear = self.non_linear(linear)
-
-        return np.argmax(non_linear)
 
 data = load_data.mnist()
 data = load_data.reshape(data)
 train_x, train_y = data[0]
 
+layer1 = SigmoidLayer(784, 30)
+layer2 = SigmoidLayer(30, 10)
+layers = [layer1, layer2]
 
+mlp = MLP(layers)
 
-layer = SigmoidLayer(784, 10)
-
+print mlp.train(train_x, train_y)
