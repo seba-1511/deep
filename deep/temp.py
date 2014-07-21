c91c3708cb03f7b8d62048bda639623c9563f77c
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-07-21 08:07:49 -0700

    mlp working on kaggle decoding training set

--- deep/temp.py
+++ deep/temp.py
@@ -1,23 +1,38 @@
 """
 Temporary file for messing around
 """
-from deep.data import load
+from deep.dataset.kaggle_decoding import KaggleDecoding
 from deep.model import mlp
 from deep.train import train
 
-data = load.kaggle_decoding()
+k = KaggleDecoding()
 
-layer1 = mlp.SigmoidLayer(375, 20)
-layer2 = mlp.SigmoidLayer(20, 1)
+layer1 = mlp.SigmoidLayer(375, 3)
+layer2 = mlp.SigmoidLayer(3, 2)
 layers = [layer1, layer2]
 
 mlp = mlp.MLP(layers)
 
-train_x, train_y = data[0]
+data = k.load()
 
-train_x = train_x[:, 179,:].reshape(594, 375, 1)
-train_y = train_y.reshape(-1, 1)
+train_x, train_y = k.train_set
+
+
+import sklearn.linear_model
+import numpy as np
+
+clf = sklearn.linear_model.LogisticRegression()
+
+print train_x.shape
+
+train_x = train_x[:, 277, :]
+train_x -= train_x.mean(0)
+train_x = np.nan_to_num(train_x / train_x.std(0))
+
 
 train.sgd((train_x, train_y), mlp)
-
 train.score((train_x, train_y), mlp)
+
+#clf.fit(train_x, train_y)
+#print clf.score(train_x, train_y)
+
