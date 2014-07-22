bfc26eea743c2931b85646906d0282d7af7eb332
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-07-22 11:15:50 -0700

    implemented batch gradient descent and made sgd a special case of bgd

--- deep/train/train.py
+++ deep/train/train.py
@@ -4,54 +4,40 @@ Model Parameter Selection
 
 import numpy as np
 
-
-def sgd(train_set, model, unsupervised=False):
+def sgd(dataset, model, unsupervised=False):
     """ stochastic gradient descent """
 
-    train_x, train_y = train_set
+    bgd(dataset, model, batch_size=1)
 
-    if unsupervised:
-        train_y = train_x
 
-    epochs = 1000
-    for epoch in range(epochs):
-        total_error = 0.0
-        for x, y in zip(train_x, train_y):
-            error = model.fprop(x) - y
-            total_error += np.sum(error**2)
+def bgd(dataset, model, batch_size = 500):
+    """ batch gradient descent """
+
+    train_x = dataset.data[0][0]
+    train_y = dataset.reshape_y(dataset.data[0][1])
+
+    num_train = train_x.shape[0]
+
+    for epoch in range(1):
+
+        for i in range(0, num_train, batch_size):
+
+            batch_x = train_x[i:i+batch_size]
+            batch_y = train_y[i:i+batch_size]
+
+            error = model.fprop(batch_x) - batch_y
             model.bprop(error)
-            model.update(.001)
-
-        print total_error
+            model.update(.1)
 
 
-def bgd(train_set, model, unsupervised=False):
+def score(dataset, model):
 
-    # TODO: implement batch gradient descent
+        valid_x, valid_y = dataset.data[0]
+        valid_y = dataset.reshape_y(valid_y)
 
-    raise NotImplementedError
+        guess = np.argmax(model.fprop(valid_x), axis=1)
+        correct = np.argmax(valid_y, axis=1)
 
+        total = np.sum(guess == correct)
 
-def gd(train_set, model, unsupervised=False):
-
-    # TODO: implement gradient descent
-
-    raise NotImplementedError
-
-
-def cgd(train_set, model, unsupervised=False):
-
-    # TODO: implement conjugate gradient descent
-
-    raise NotImplementedError
-
-
-def score(valid_set, model):
-
-        valid_x, valid_y = valid_set
-
-        correct = 0.0
-        for x, y in zip(valid_x, valid_y):
-            if np.argmax(model.fprop(x)) == np.argmax(y):
-                correct += 1.0
-        print correct / len(valid_x)
+        print float(total) / len(valid_x)
