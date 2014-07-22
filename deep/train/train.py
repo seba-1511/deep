ac298400fa6a01a1a972000ec0c03632f0f591a4
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-07-22 11:46:35 -0700

    updated training to reflect dataset changes. removed kaggle dataset

--- deep/train/train.py
+++ deep/train/train.py
@@ -10,20 +10,15 @@ def sgd(dataset, model, unsupervised=False):
     bgd(dataset, model, batch_size=1)
 
 
-def bgd(dataset, model, batch_size = 500):
+def bgd(dataset, model, batch_size=500):
     """ batch gradient descent """
-
-    train_x = dataset.data[0][0]
-    train_y = dataset.reshape_y(dataset.data[0][1])
-
-    num_train = train_x.shape[0]
 
     for epoch in range(1):
 
-        for i in range(0, num_train, batch_size):
+        for i in range(0, dataset.train_size, batch_size):
 
-            batch_x = train_x[i:i+batch_size]
-            batch_y = train_y[i:i+batch_size]
+            batch_x = dataset.train_x[i:i+batch_size]
+            batch_y = dataset.train_bin_y[i:i+batch_size]
 
             error = model.fprop(batch_x) - batch_y
             model.bprop(error)
@@ -32,12 +27,9 @@ def bgd(dataset, model, batch_size = 500):
 
 def score(dataset, model):
 
-        valid_x, valid_y = dataset.data[0]
-        valid_y = dataset.reshape_y(valid_y)
-
-        guess = np.argmax(model.fprop(valid_x), axis=1)
-        correct = np.argmax(valid_y, axis=1)
+        guess = np.argmax(model.fprop(dataset.valid_x), axis=1)
+        correct = dataset.valid_y
 
         total = np.sum(guess == correct)
 
-        print float(total) / len(valid_x)
+        print float(total) / dataset.valid_size
