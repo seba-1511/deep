678d434d5a417fbe09590dee69a7fd7e50e01c1f
GabrielPereyra <gbrl.pereyra@gmail.com>
2014-07-29 18:57:27 -0700

    removed old unit tests and made skeleton for class units

--- deep/test/test_mlp.py
+++ deep/test/test_mlp.py
@@ -2,67 +2,48 @@ import numpy as np
 from deep.model.mlp import Layer
 from deep.model.mlp import LinearLayer
 from deep.model.mlp import SigmoidLayer
+from deep.model.mlp import LinearConvolutionLayer
+from deep.model.mlp import SigmoidConvolutionLayer
 from deep.model.mlp import MultiLayerPerceptron
 
 
-def test_layer():
-    """ testing layer """
+class TestLayer():
 
-    # check if activations below are saved
-    layer = Layer()
-    layer.fprop(np.arange(10))
+    def __init__():
 
-    assert np.all(layer.activation_below == np.arange(10))
+        raise NotImplementedError
 
 
-def test_linear_layer():
-    """ testing linear layer """
+class TestLinearLayer():
 
-    # check if weight shapes are initialized correctly
-    linear_layer = LinearLayer(10, 1)
-    assert linear_layer.weights.shape == (10, 1)
-    assert linear_layer.bias.shape == (1,)
+    def __init__():
 
-    # check if linear fprop works with identity matrix as weights
-    linear_layer.weights = np.eye(10)
-    assert np.all(linear_layer.fprop(np.arange(10)) == np.arange(10))
-
-    # check if activations are stored
-    assert np.all(linear_layer.activation_below == np.arange(10))
-    assert np.all(linear_layer.activation_linear == np.arange(10))
+        raise NotImplementedError
 
 
-def test_sigmoid_layer():
-    """ testing sigmoid layer """
+class TestSigmoidLayer():
 
-    # check if weights are initialized correctly
-    sigmoid_layer = SigmoidLayer(10, 1)
-    assert sigmoid_layer.weights.shape == (10, 1)
-    assert sigmoid_layer.bias.shape == (1,)
+    def __init__():
 
-    # check if sigmoid fprop works with identity matrix as weights
-    sigmoid_layer.weights = np.eye(10)
-    assert np.all(sigmoid_layer.fprop(np.arange(10)) ==
-                  sigmoid_layer.sigmoid(np.arange(10)))
-
-    # check if activations are stored
-    assert np.all(sigmoid_layer.activation_below == np.arange(10))
-    assert np.all(sigmoid_layer.activation_linear == np.arange(10))
-    assert np.all(sigmoid_layer.activation_non_linear ==
-                  sigmoid_layer.sigmoid(np.arange(10)))
+        raise NotImplementedError
 
 
-def test_mlp():
-    """ testing mlp """
+class TestLinearConvolutionLayer():
 
-    # check if weights are initialized correctly
-    multi_layer = MultiLayerPerceptron([10, 5, 2])
-    assert multi_layer.layers[0].weights.shape == (10, 5)
-    assert multi_layer.layers[1].weights.shape == (5, 2)
+    def __init__():
 
-    # initialize with list of layers
-    layer1 = SigmoidLayer(10, 5)
-    layer2 = SigmoidLayer(5, 2)
-    multi_layer = MultiLayerPerceptron([layer1, layer2])
-    assert multi_layer.layers[0].weights.shape == (10, 5)
-    assert multi_layer.layers[1].weights.shape == (5, 2)
+        raise NotImplementedError
+
+
+class TestSigmoidConvolutionLayer():
+
+    def __init__():
+
+        raise NotImplementedError
+
+
+class TestMultiLayerPerceptron():
+
+    def __init__():
+
+        raise NotImplementedError
