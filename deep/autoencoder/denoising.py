cbe7f8e519a76bbab2426f5269dc6bfce3197e4f
Gabriel Pereyra <gbrl.pereyra@gmail.com>
2015-02-03 21:17:56 -0800

    working denoising ae

--- deep/autoencoder/denoising.py
+++ deep/autoencoder/denoising.py
@@ -1,20 +1,46 @@
-from deep.autoencoder.base import TiedAE
-from deep.layers.denoising import DenoisingLayer
-from deep.corruptions.base import SaltAndPepper
+# -*- coding: utf-8 -*-
+"""
+    deep.autoencoder.denoising
+    --------------------------
 
-corruption = SaltAndPepper()
+    Implements a denoising autoencoder.
+
+    :references: pylearn2 (corruptions module)
+
+    :copyright: (c) 2014 by Gabriel Pereyra.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import theano.tensor as T
+
+from deep.autoencoder.base import TiedAE
+from deep.layers import DenoisingLayer
+from deep.corruptions.base import SaltAndPepper
+from deep.utils.base import theano_compatible
+from deep.activations.base import Sigmoid
+from deep.fit.base import Fit
+from deep.costs.base import BinaryCrossEntropy
+from deep.updates.base import GradientDescent
 
 
 class TiedDenoisingAE(TiedAE, DenoisingLayer):
 
-    def __init__(self, corruption=corruption):
-        super(TiedDenoisingAE, self).__init__()
-        self.corrupt = corruption.corrupt
+    def __init__(self, activation=Sigmoid(), learning_rate=10,
+                 n_hidden=100, n_iter=10, batch_size=100, _fit=Fit(),
+                 _cost=BinaryCrossEntropy(), update=GradientDescent(),
+                 corruption=SaltAndPepper()):
+        super(TiedDenoisingAE, self).__init__(activation, learning_rate,
+                                              n_hidden, n_iter, batch_size,
+                                              _fit, _cost, update)
+        self.corrupt = corruption
 
+    #: can we push this down into the layer? (mro is wrong)
+    @theano_compatible
+    def transform(self, X, noisy=True):
+        if noisy:
+            X = self.corrupt(X)
+        return self.activation(T.dot(X, self.W) + self.b)
 
-if __name__ == '__main__':
-    tdae = TiedDenoisingAE()
-    from deep.datasets import load_mnist
-    X, y = load_mnist()[0]
-
-    tdae.fit(X)
+    @theano_compatible
+    def reconstruct(self, X, noisy=True):
+        return self.inverse_transform(self.transform(X))
