c78ef05fa9d3e738b35d577a7970246714961952
Gabriel Pereyra <gbrl.pereyra@gmail.com>
2015-02-09 14:52:37 -0800

    removed plot corruption

--- deep/plot/base.py
+++ deep/plot/base.py
@@ -25,38 +25,12 @@ def setup_plot(func):
 
 
 @setup_plot
-def plot_data(X, filename=None):
+def plot_image_data(X, filename=None):
 
     for index, x in enumerate(X):
         plt.setp(plt.subplot(10, 10, index), xticks=[], yticks=[])
         plt.imshow(x.reshape(28, 28), cmap=plt.get_cmap('gray'),
                    interpolation='nearest')
-
-
-@setup_plot
-def plot_corruption(X, corrupt=SaltAndPepper, filename=None):
-
-    plt.figure(figsize=(10, 10))
-    plt.subplots_adjust(left=0.0, bottom=0.0, right=1.0,
-                        top=1.0, wspace=0.0, hspace=0.0)
-
-    corruptors = [corrupt(level/10.) for level in range(1, 10)]
-
-    for row, x in enumerate(X):
-
-        corrupted = [corrupt(x) for corrupt in corruptors]
-        corrupted = [x] + corrupted
-
-        for col, c in enumerate(corrupted):
-            plt.setp(plt.subplot(10, 10, row*10+col+1), xticks=[], yticks=[])
-            plt.imshow(c.reshape(28, 28), cmap=plt.get_cmap('gray'),
-                       interpolation='nearest')
-
-    if filename:
-        plt.savefig(filename)
-    else:
-        plt.show()
-
 
 def plot_layer(layer, filename=None):
     from deep.layers import ConvolutionLayer
