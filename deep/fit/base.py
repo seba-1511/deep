cleaned up noisy/silent corruption logic (still need to fix fit)

deep/fit/base.py
@@ -15,7 +15,7 @@ import time
 import numpy as np
 import theano.tensor as T
 from abc import abstractmethod
-from theano import shared, config, function
+from theano import function
 
 from deep.datasets import SupervisedData, UnsupervisedData
 
@@ -45,11 +45,17 @@ class Iterative(Fit):
         if isinstance(dataset, SupervisedData):
             y = T.lvector()
             train_score = model._symbolic_score(x, y)
+            updates = model.updates(x, y)
             train_givens = dataset.givens(x, y, self.batch_size)
 
             if self.valid is not None:
+
+                #: remove noise to compile clean score
+                for layer in model.layers:
+                    layer.corruption = None
+
                 valid_score = model._symbolic_score(x, y)
-                valid_givens = dataset.givens(x, y, self.batch_size)
+                valid_givens = self.valid.givens(x, y, self.batch_size)
 
         if isinstance(dataset, UnsupervisedData):
             train_score = model._symbolic_score(x)
@@ -58,7 +64,7 @@ class Iterative(Fit):
 
             if self.valid is not None:
                 valid_score = model._symbolic_score(x)
-                valid_givens = dataset.givens(x, self.batch_size)
+                valid_givens = self.valid.givens(x, self.batch_size)
 
         train = function(index, train_score, None, updates, train_givens)
         n_train_batches = len(dataset) / self.batch_size
@@ -80,13 +86,12 @@ class Iterative(Fit):
             if self.valid is not None:
                 valid_cost = np.mean(valid_costs)
 
-            print("[%s] Iteration %d, train = %.2f, valid = %.2f, time = %.2fs"
+            print("[%s] Iteration %d, train = %.2f, valid = %.4f, time = %.2fs"
                   % (type(model).__name__, iteration, train_cost, valid_cost, elapsed))
 
             dataset.update()
 
         return model
-
 
 
 class EarlyStopping(Fit):

deep/models/nn.py
@@ -22,56 +22,27 @@ from deep.updates.base import GradientDescent
 from deep.datasets import SupervisedData
 
 class NN(object):
-    """A Feed Forward Neural Network is composed of one or more layers,
-    each parametrized by a weight matrix and a biases vector. The weights
-    are learned, typically through an iterative optimization procedure, and
-    then the network can be used to make predictions on new data.
 
-    Example::
-
-        from deep.datasets import load_mnist
-        mnist = load_mnist()
-        X_train, y_train = mnist[0]
-        X_test, y_test = mnist[2]
-
-        from deep.models import FeedForwardNN
-        clf = FeedForwardNN()
-
-        clf.fit(X_train, y_train)
-        print clf.costs(X_test, y_test)
-
-    :references:
-
-    :param activations: activations for each layer.
-    :param layer_sizes: number of units in hidden layers.
-    :param learning_rate: step size used in fit.
-    :param n_iter: number of iterations to run fit.
-    :param batch_size: size of batches used for fit.
-    :param cost: the function that defines the training error.
-    :param update: the way that the parameters are updated.
-    :param costs: the costs that is printed during training.
-    :param fit: the fit method to use when calling fit().
-    """
-    def __init__(self, layers=None, learning_rate=10, cost=NegativeLogLikelihood(),
-                 update=GradientDescent(), fit=Iterative()):
-
-        self.layers = layers or []
+    def __init__(self, layers, learning_rate=10, update=GradientDescent(),
+                 fit=Iterative(), cost=NegativeLogLikelihood()):
+        self.layers = layers
         self.learning_rate = learning_rate
+        self.update = update
         self.fit_method = fit
         self.cost = cost
-        self.update = update
 
+    #: this is only used to compile predict_proba
+    #: do this in fit?
     x = T.matrix()
-    y = T.lvector()
     _predict_proba_function = None
 
     @property
     def params(self):
         return [param for layer in self.layers for param in layer.params]
 
-    @property
-    def updates(self):
-        cost = self.cost(self._symbolic_predict_proba(self.x), self.y)
+    #: can we move this to updates?
+    def updates(self, x, y):
+        cost = self.cost(self._symbolic_predict_proba(x), y)
 
         #: add regularizer to network
 
@@ -79,6 +50,18 @@ class NN(object):
         for param in self.params:
             updates.extend(self.update(cost, param, self.learning_rate))
         return updates
+
+    def _symbolic_predict(self, x):
+        return T.argmax(self._symbolic_predict_proba(x), axis=1)
+
+    def _symbolic_predict_proba(self, X):
+        for layer in self.layers:
+            X = layer._symbolic_transform(X)
+        return X
+
+    def _symbolic_score(self, x, y):
+        cost = PredictionError()
+        return cost(self._symbolic_predict(x), y)
 
     def predict(self, X):
         return np.argmax(self.predict_proba(X), axis=1)
@@ -91,19 +74,6 @@ class NN(object):
 
     def score(self, X, y):
         return np.mean(self.predict(X) == y)
-
-    def _symbolic_predict(self, x):
-        return T.argmax(self._symbolic_predict_proba(x), axis=1)
-
-    def _symbolic_predict_proba(self, X):
-        """A Theano expression representing a class distribution."""
-        for layer in self.layers:
-            X = layer._symbolic_transform(X)
-        return X
-
-    def _symbolic_score(self, x, y):
-        cost = PredictionError()
-        return cost(self._symbolic_predict(x), y)
 
     def fit(self, X, y=None):
         #: should we just remove X, y and take a dataset?